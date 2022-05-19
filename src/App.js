ced8143c6312fd3ce7f5046ab25779fa8ae7e4c3
Emil Terrero <emil@Emils-MBP.lan>
2022-05-19 11:50:17 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,67 +1,63 @@
-import {useState} from 'react';
 import './App.css';
+import {useState} from 'react';
 
 const App = () => {
-const [text, setText] = useState('');
-const [prompts, setPrompts] = useState([]);
-  
-const formSubmit = (e) => {
-    e.preventDefault()
-    try {
-      fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
-      },
-      body: JSON.stringify({
-        prompt: `${text}`,
-        temperature: 0.5,
-        max_tokens: 64,
-        top_p: 1.0,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-       }
-       ),
-  }
-  )
-      .then(res => res.json())
-      .then(data => setPrompts([data, ...prompts]))
-      setText('')
+    const [text, setText] = useState('');
+    const [prompts, setPrompts] = useState([]);
+    const [fullRes, setRes] = useState({});
 
-    } catch (error) {
-      console.error(error)
+
+    const formSubmit = (e) => {
+        e.preventDefault()
+        try {
+            fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+                method: "POST",
+                headers: {
+                    "Access-Controll-Allow-Origin": "*",
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+                },
+                body: JSON.stringify({
+                    prompt: `${text}`,
+                    temperature: 0.5,
+                    max_tokens: 64,
+                    top_p: 1.0,
+                    frequency_penalty: 0.0,
+                    presence_penalty: 0.0,
+                }
+                ),
+            }
+            )
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                })
+            setText('')
+
+        } catch (error) {
+            console.error(error)
+        }
     }
-  }
 
 
-  return (
-    <div className="App">
-      <h1>Open AI App</h1>
+    return (
+        <div className="App">
+        <h1>Open AI App</h1>
 
-      <div className='content'>
+        <div className='input-field'>
 
-      <form onSubmit={formSubmit} id='submit-form'>
-      <textarea type='text' id='input' onChange={(e) => setText(e.currentTarget.value)} value={text} />
-      <button className='submit=btn' type='submit'>Submit
-      </button>
-      </form>
-      <div>
-      {prompts.map((prompt) => {
-        console.log(prompt)
-        return (
-          <div>
-          <h1>Response:</h1>
-          <p>{prompt.choices[0].text}</p>
-          </div>
-        )
-      })}
-      </div>
+        <form onSubmit={formSubmit} id='submit-form'>
+        <textarea type='text' id='input' onChange={(e) => setText(e.currentTarget.value)} value={text} />
+        <button className='submit=btn' type='submit'>Submit
+        </button>
+        </form>
 
-      </div>
+        </div>
+        <div className='responses'>
+        </div>
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
